8068f9a4526fc7fd2b455449d6969b0657b381e7
Ruhan-Royy <97819615+Ruhan-Royy@users.noreply.github.com>
2022-05-03 17:32:25 +0530

    Add files via upload
    It encrypts the sentence you input in. It can also work as a decrypting tool with a few slight changes

---
+++ encripter.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+key = -5
+newMessage = ''
+
+message = input('Please enter a message:')
+
+for character in message:
+ if character in alphabet:
+  position = alphabet.find(character)
+  newPosition = (position + key) % 26
+  newCharacter = alphabet[newPosition]
+  newMessage += newCharacter
+else:
+  newMessage += character
+  
+print('Your new message is' , newMessage)
+  
