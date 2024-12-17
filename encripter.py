1750e05f02d8975e53d40a20a8271e763a1dc05e
Ruhan-Roy <89984651+Ruhan-Roy@users.noreply.github.com>
2024-12-17 09:11:26 +0530

    encripter.py

--- encripter.py
+++ encripter.py
@@ -7,13 +7,13 @@ newMessage = ''
 message = input('Please enter a message:')
 
 for character in message:
- if character in alphabet:
-  position = alphabet.find(character)
-  newPosition = (position + key) % 26
-  newCharacter = alphabet[newPosition]
-  newMessage += newCharacter
-else:
-  newMessage += character
-  
-print('Your new message is' , newMessage)
-  
+    if character in alphabet:
+        position = alphabet.find(character)
+        newPosition = (position + key) % 26
+        newCharacter = alphabet[newPosition]
+        newMessage += newCharacter
+    else:
+        newMessage += character  
+
+print('Your new message is', newMessage)
+
